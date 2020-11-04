154c12832706019ccc7bac6d1ddb06e497c8ad8b
Janine Parham <jparhamdev@gmail.com>
2020-11-04 10:25:47 -0600

    first commit

---
+++ src/main/java/com/example/DAy23Lab/DAy23LabApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAy23Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAy23LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DAy23LabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DAy23Lab/DAy23LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAy23Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAy23LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
